367222cea8bf77e2039be94bd1f7726dd1d930a4
timour akhounov <timon1983@yandex.ru>
2021-10-20 16:01:00 +0500

    add NamespaceFilter

---
+++ src/main/java/com/example/KafkaModificateXML/service/NamespaceFilter.java
@@ -0,0 +1,20 @@
+package com.example.KafkaModificateXML.service;
+
+import org.xml.sax.Attributes;
+import org.xml.sax.SAXException;
+import org.xml.sax.helpers.XMLFilterImpl;
+
+public class NamespaceFilter extends XMLFilterImpl {
+
+    private static final String NAMESPACE = "http://svn.msk.trd.ru/xsd/fixml";
+
+    @Override
+    public void endElement(String uri, String localName, String qName) throws SAXException {
+        super.endElement(NAMESPACE, localName, qName);
+    }
+
+    @Override
+    public void startElement(String uri, String localName, String qName, Attributes attributes) throws SAXException {
+        super.startElement(NAMESPACE, localName, qName, attributes);
+    }
+}
