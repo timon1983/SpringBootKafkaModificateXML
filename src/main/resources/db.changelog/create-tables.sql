7f15b7b745afebe2002e696e0f41bdd48542578f
timour akhounov <timon1983@yandex.ru>
2021-10-26 17:53:08 +0500

    edit liquibase , add changelog-master.xml

---
+++ src/main/resources/db.changelog/create-tables.sql
@@ -0,0 +1,21 @@
+create table if not exists data(
+                                  id serial not null,
+                                  primary key (id),
+                                  type varchar(45) not null,
+                                  in_value varchar(45),
+                                  out_value varchar(45) ,
+                                  version int not null
+    );
+create table if not exists topics(
+                                  id serial not null,
+                                  primary key (id),
+                                  read_topic varchar(100) not null,
+                                  send_topic varchar(100) not null
+    );
+
+create table if not exists messages(
+                                  id serial not null,
+                                  primary key (id),
+                                  in_message varchar ,
+                                  out_message varchar
+    );
